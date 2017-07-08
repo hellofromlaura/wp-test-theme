f303161413bec4f7fce73173e19571fe39f84c10
Laura Wolfart <laurawolfart@gmail.com>
2017-07-07 23:39:18 -0400

    sidebar

--- wp-content/themes/lauras-portfolio/page.php
+++ wp-content/themes/lauras-portfolio/page.php
@@ -1,42 +1,25 @@
 <?php get_header(); ?>
-	<div class="container">
-		<div class="main">
+	<div class="main">
+		<div class="container">
 			<?php if(have_posts()) : ?>
 				<?php while(have_posts()): the_post(); ?>
 					<article class="post">
 						<h3>
-						<a href="<?php the_permalink(); ?>">
 							<?php the_title(); ?>
-						</a>
 						</h3>
-						<div class="meta">
-						Created By <?php the_author(); ?> on <?php the_time('F j, Y g:i a'); ?>
-						</div>
 						<?php if(has_post_thumbnail()) : ?>
 							<div class="post-thumbnail">
 								<?php the_post_thumbnail(); ?>
 							</div>
 						<?php endif; ?>
-						<?php the_excerpt(); ?>
+						<?php the_content(); ?>
 						<br>
-						<a class="button" href="<?php the_permalink(); ?>">
-							Read More
-						</a>
 					</article>
 				<?php endwhile; ?>
 			<?php else : ?>
 				<?php echo wpautop('Sorry, No posts were found'); ?>
 			<?php endif; ?>
-	</div>
-
-	<div class="sidebar">
-		<?php if(is_active_sidebar('sidebar')) : ?>
-			<?php dynamic_sidebar('sidebar'); ?>
-		<?php endif; ?>
-	</div>
-
-	<div class="clr">
-
+		</div>
 	</div>
 
 <?php get_footer(); ?>

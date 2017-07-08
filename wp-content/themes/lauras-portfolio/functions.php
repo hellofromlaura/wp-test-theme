4dcc36ee95044d46bff673536374759f9dbeba3b
Laura Wolfart <laurawolfart@gmail.com>
2017-07-07 23:17:00 -0400

    navigation implemented

--- wp-content/themes/lauras-portfolio/functions.php
+++ wp-content/themes/lauras-portfolio/functions.php
@@ -1,7 +1,20 @@
 <?php
-function simple_theme_setup() {
-  add_theme_support('post-thumbnails');
+function simple_theme_setup(){
+	// Featured Image Support
+	add_theme_support('post-thumbnails');
+
+	// Menus
+	register_nav_menus(array(
+		'primary'	=> __('Primary Menu')
+	));
 }
 
-add_action('after_setup_theme', '');
-?>
+add_action('after_setup_theme', 'simple_theme_setup');
+
+
+// Excerpt Length
+function set_excerpt_length(){
+	return 60;
+}
+
+add_filter('excerpt_length', 'set_excerpt_length');
